537bac1c97995d793a96f69eefca9fbf1c5a2307
Semyon Scherbinin <s.scherbinin@parcsis.org>
2014-06-28 20:01:50 +0400

    add dal

---
+++ tofo.dal/Todo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace todo.dal
+{
+	public class Todo
+	{
+		public Guid Id { get; internal set; }
+		public string Text { get; set; }
+		public bool Done { get; set; }
+	}
+}
---
+++ tofo.dal/TodoDBContext.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace todo.dal
+{
+	public class TodoDBContext : DbContext
+	{
+		public DbSet<Todo> Todoes { get; set; }
+	}
+}
