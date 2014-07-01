44c596c573c2de86d14fb588812fe0679bcd6471
Semyon Scherbinin <s.scherbinin@parcsis.org>
2014-07-01 23:45:57 +0400

    publish to azure

--- tofo.dal/TodoDBContext.cs
+++ tofo.dal/TodoDBContext.cs
@@ -1,6 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.Common;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Data.Entity.Infrastructure.DependencyResolution;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,5 +19,29 @@ namespace todo.dal
 
 		}
 		public DbSet<Todo> Todoes { get; set; }
+	}
+
+	public class MyConfiguration : DbConfiguration
+	{
+		public MyConfiguration()
+		{
+			AddDependencyResolver(new SingletonDependencyResolver<IManifestTokenResolver>(new SqlAzureManifestTokenResolver()));
+		}
+	}
+
+	public class SqlAzureManifestTokenResolver : IManifestTokenResolver
+	{
+		public string ResolveManifestToken(DbConnection connection)
+		{
+			// customize your logic here if needed
+			if (connection is SqlConnection)
+			{
+				return "2012.Azure";
+			}
+			else
+			{
+				return new DefaultManifestTokenResolver().ResolveManifestToken(connection);
+			}
+		}
 	}
 }
