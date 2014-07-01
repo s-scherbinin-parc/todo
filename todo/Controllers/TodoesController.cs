2e70f2afdfc53c3865b559c947aebb2dae4f9dfa
Semyon Scherbinin <s.scherbinin@parcsis.org>
2014-07-01 22:31:33 +0400

    add and update todoes

---
+++ todo/Controllers/TodoesController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web.Http;
+using todo.dal;
+
+namespace todo.Controllers
+{
+	public class TodoesController : ApiController
+	{
+		TodoDBContext _context = new TodoDBContext();
+
+		public IEnumerable<Todo> Get()
+		{
+			return _context.Todoes.ToArray();
+		}
+
+		public Todo Post(Todo todo)
+		{
+			_context.Todoes.Add(todo);
+			_context.SaveChanges();
+			return todo;
+		}
+
+		public Todo Put(Todo todo)
+		{
+			_context.Entry<Todo>(todo).State = EntityState.Modified;
+			_context.SaveChanges();
+			return todo;
+		}
+
+		protected override void Dispose(bool disposing)
+		{
+			_context.Dispose();
+			base.Dispose(disposing);
+		}
+	}
+}
--- todo/Scripts/app/controller.js
+++ todo/Scripts/app/controller.js
@@ -6,10 +6,24 @@ phonecatApp.factory('Todo', function ($resource) {
 			method:"GET",
 			isArray:true
 		},
+		push: {
+			method: "POST"
+		},
+		put: {
+			method: "PUT"
+		}
 	});
 	return resource;
 });
 
 phonecatApp.controller('TodoCtrl', function ($scope, Todo) {
 	$scope.todoes = Todo.get();
+	$scope.add = function (todo) {
+		Todo.push(todo, function (r) {
+			$scope.todoes.push(r);
+		});
+	}
+	$scope.update = function (todo) {
+		Todo.put(todo);
+	}
 });
