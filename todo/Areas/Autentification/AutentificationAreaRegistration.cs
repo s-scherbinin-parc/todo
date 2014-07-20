f766604d0316d0394a9b699c2abe2f1553ca2692
Semyon Scherbinin <s.scherbinin@parcsis.org>
2014-07-20 19:55:31 +0400

    Added login form template
    Conflicts:
    	todo/Web.config

--- todo/Areas/Autentification/AutentificationAreaRegistration.cs
+++ todo/Areas/Autentification/AutentificationAreaRegistration.cs
@@ -1,24 +1,28 @@
 ﻿using System.Web.Mvc;
+using System.Web.Optimization;
 
 namespace todo.Areas.Autentification
 {
-    public class AutentificationAreaRegistration : AreaRegistration 
-    {
-        public override string AreaName 
-        {
-            get 
-            {
-                return "Autentification";
-            }
-        }
+	public class AutentificationAreaRegistration : AreaRegistration
+	{
+		public override string AreaName
+		{
+			get
+			{
+				return "Autentification";
+			}
+		}
 
-        public override void RegisterArea(AreaRegistrationContext context) 
-        {
-            context.MapRoute(
-                "Autentification_default",
-                "Autentification/{controller}/{action}/{id}",
-                new { action = "Index", id = UrlParameter.Optional }
-            );
-        }
-    }
+		public override void RegisterArea(AreaRegistrationContext context)
+		{
+			context.MapRoute(
+				"Autentification_default",
+				"Autentification/{controller}/{action}/{id}",
+				new { action = "Index", id = UrlParameter.Optional }
+			);
+
+			BundleTable.Bundles.Add(new StyleBundle("~/Autentification/Content/css")
+				.Include("~/Areas/Autentification/Content/login.css"));
+		}
+	}
 }
