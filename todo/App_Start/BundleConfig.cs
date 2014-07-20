0e3fa00a8754841be4ecae6e38404b221ebcf3da
Semyon Scherbinin <s.scherbinin@parcsis.org>
2014-07-20 19:07:49 +0400

    add bower.json

--- todo/App_Start/BundleConfig.cs
+++ todo/App_Start/BundleConfig.cs
@@ -1,4 +1,5 @@
-﻿using System.Web;
+﻿using System;
+using System.Web;
 using System.Web.Optimization;
 
 namespace todo
@@ -9,38 +10,14 @@ namespace todo
 		public static void RegisterBundles(BundleCollection bundles)
 		{
 			bundles.Add(new ScriptBundle("~/bundles/app").Include(
-						"~/Scripts/app/*.js"));
+						"~/Content/controller.js"));
 
 			bundles.Add(new ScriptBundle("~/bundles/angularjs").Include(
-						"~/Scripts/angular.js",
-						"~/Scripts/angular-route.js",
-						"~/Scripts/angular-resource.js"));
-
-			bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-						"~/Scripts/jquery-{version}.js"));
-
-			bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
-						"~/Scripts/jquery-ui-{version}.js"));
-
-			bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-						"~/Scripts/jquery.unobtrusive*",
-						"~/Scripts/jquery.validate*"));
+						"~/Content/vendor/angular/angular.js",
+						"~/Content/vendor/angular-route/angular-route.js",
+						"~/Content/vendor/angular-resource/angular-resource.js"));
 
 			bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-
-			bundles.Add(new StyleBundle("~/Content/themes/base/css").Include(
-						"~/Content/themes/base/jquery.ui.core.css",
-						"~/Content/themes/base/jquery.ui.resizable.css",
-						"~/Content/themes/base/jquery.ui.selectable.css",
-						"~/Content/themes/base/jquery.ui.accordion.css",
-						"~/Content/themes/base/jquery.ui.autocomplete.css",
-						"~/Content/themes/base/jquery.ui.button.css",
-						"~/Content/themes/base/jquery.ui.dialog.css",
-						"~/Content/themes/base/jquery.ui.slider.css",
-						"~/Content/themes/base/jquery.ui.tabs.css",
-						"~/Content/themes/base/jquery.ui.datepicker.css",
-						"~/Content/themes/base/jquery.ui.progressbar.css",
-						"~/Content/themes/base/jquery.ui.theme.css"));
 			//BundleTable.EnableOptimizations = true;
 		}
 	}
